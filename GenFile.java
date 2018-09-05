6c60e2d90675c83cf0e9d88f2a983558e2717db7
justdan325 <dannyjames16@hotmail.com>
2018-09-05 17:06:31 -0400

    Added fields to GenFile.

--- GenFile.java
+++ GenFile.java
@@ -5,12 +5,17 @@
 *@version 1.0
 */
 
+import java.nio.file;
+
 public class GenFile
 {
 	/*FIELDS*/
 	String canonicalPath;		//canonical path of file
-	
-	
+	String modStamp;			//holds time stamp of last modiifiication time
+	FileTime lastMod;			//holds most recent modification time
+	boolean canUpdate;		//true if the file can be updated
+	boolean canDel;				//true if the file can be deleted
+	boolean canRead;			//true if file can be read (and thus copied) 
 	
 	/**
 	*Constructor
