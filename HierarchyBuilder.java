cfc8af45a61442f305c1e523bf6c71a26ce78190
justdan325 <dannyjames16@hotmail.com>
2018-09-10 15:11:32 -0400

    Added Class File for HirearchyBuilder.

---
+++ HierarchyBuilder.java
@@ -0,0 +1,9 @@
+/**
+*Class will build a hierarcy of Database and GenFile objects from an existing file system hierarcy or from a database file.
+*
+*@author Dan Martineau
+*@version 1.0
+*/
+
+public class HierarchyBuilder
+{}
