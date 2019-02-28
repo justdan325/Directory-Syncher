8b17d8620e23ed545a5a10def98f3f694065551a
justdan325 <dannyjames16@hotmail.com>
2019-02-28 16:05:03 -0500

    Finished Node and tested it.

--- Node.java
+++ Node.java
@@ -1,15 +1,18 @@
 /**
 *Class represents a node for a synchrc object
-*Nodes are Immutable
+*Nodes should form a binary search tree
 *
 *@author Dan Martineau
-*@version 1.0
+*@version 2.0
 */
 
 public class Node
 {
 	/*FIELDS*/
 	String path;			//String for canonical path of Node
+	String name;			//Name of file from path
+	Node left;				//Node to the left in the bin search tree
+	Node right;			//Node to the right in the bin search tree
 	boolean read;		//read permission
 	boolean modify;		//modify permission
 	boolean delete;		//delete permission
@@ -27,6 +30,11 @@ public class Node
 		this.read = read;
 		this.modify = modify;
 		this.delete = delete;
+		
+		name = FileCMD.getName(path);
+		
+		left = null;
+		right = null;
 	}
 	
 	/*ACCESSORS*/
@@ -38,6 +46,15 @@ public class Node
 	protected String getPath()
 	{
 		return path;
+	}
+	
+	/**
+	*Return name of file
+	*@return String
+	*/
+	protected String getName()
+	{
+		return name;
 	}
 	
 	/**
@@ -65,5 +82,94 @@ public class Node
 	protected boolean getDelete()
 	{
 		return delete;
+	}
+	
+	/**
+	*Returns left child of this Node
+	*@return Node
+	*/
+	protected Node getLeft()
+	{
+		return left;
+	}
+	
+	/**
+	*Returns right child of this Node
+	*@return Node
+	*/
+	protected Node getRight()
+	{
+		return right;
+	}
+	
+	/**
+	*Searches for node by path and returns it
+	*@param path of desired node
+	*@return reference to node/null if doesn't exist
+	*/
+	protected Node getNode(String nodePath)
+	{
+		Node desired = null;	//holder for desired node to return
+		Node curr;					//current node being examined
+		String nodeName;		//name of file at given path
+		int status;					//holder for value of comapreTo comparison
+		
+		nodeName = FileCMD.getName(nodePath);
+		
+		status = name.compareTo(nodeName);
+		
+		if(status == 0 && path.equals(nodePath))
+		{
+			desired = this;
+		}
+		else
+		{
+			if(status <= 0 && right != null)
+			{
+				if(right.getPath().equals(nodePath))
+					desired = right;
+				else
+					desired = right.getNode(nodePath);
+			}
+			else if(left != null)
+			{
+				if(left.getPath().equals(nodePath))
+					desired = left;
+				else
+					desired = left.getNode(nodePath);
+			}
+		}
+		
+		return desired;
+	}
+	
+	/*MUTATORS*/
+	
+	/**
+	*Recursive--Adds a node where appropriate in the bin search tree
+	*Sorts nodes by alphabetical order of name
+	*@param Node
+	*/
+	protected void addNode(Node toAdd)
+	{
+		int status;		//holder for value of comapreTo comparison
+		
+		status = name.compareTo(toAdd.getName());
+		
+		//if toAdd is after this node or equal to it
+		if(status <= 0)
+		{
+			if(right == null)
+				right = toAdd;
+			else
+				right.addNode(toAdd);
+		}
+		else
+		{
+			if(left == null)
+				left = toAdd;
+			else
+				left.addNode(toAdd);
+		}
 	}
 }
--- UnitTest.java
+++ UnitTest.java
@@ -7,8 +7,16 @@ public class UnitTest
 {
 	public static void main(String[] args)
 	{
-		SynchModule test = new SynchModule("/home/dan/Programming Projects/FileSynch/testDir", "/home/dan/Programming Projects/FileSynch/newDir", SynchModule.DEFAULT_SYNCHRC, true, true, true);
+		//SynchModule test = new SynchModule("/home/dan/Programming Projects/FileSynch/testDir", "/home/dan/Programming Projects/FileSynch/newDir", SynchModule.DEFAULT_SYNCHRC, true, true, true);
 		
-		Prin.tln("\n\n\n" + test.getLog());
+		//Prin.tln("\n\n\n" + test.getLog());
+		
+		/*Node root = new Node("/home/dan/Programming Projects/FileSynch/testDir/b", false, false, false);
+		root.addNode(new Node("/home/dan/Programming Projects/FileSynch/testDir/a", false, false, false));
+		root.addNode(new Node("/home/dan/Programming Projects/FileSynch/testDir/3/b", false, false, false));
+		root.addNode(new Node("/home/dan/Programming Projects/FileSynch/testDir/c", false, false, false));
+		
+		try{Prin.tln(root.getNode("/home/dan/Programming Projects/FileSynch/testDir/3/b").getPath());}
+		catch(Exception e){e.printStackTrace();}*/
 	}
 }
