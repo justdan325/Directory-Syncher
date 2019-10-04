bcbe3b767b6668fcef27527aa157a741abd2e307
justdan325 <dannyjames16@hotmail.com>
2019-10-03 21:25:22 -0400

    Refactored and debugged Main.

--- Main.java
+++ Main.java
@@ -6,27 +6,56 @@
 *@version 1.5
 */
 
+import java.util.regex.*;
 import java.io.File;
 import java.util.Date;
 
 public class Main
 {
-	private static String log;
+	private static boolean read;
+	private static boolean mod;
+	private static boolean del;
 	private static boolean verbose;
+	private static boolean unidirectional;
+	private static boolean saveLog;
+	private static int initialLogLen;
+	private static String dir1;
+	private static String dir2;
+	private static String log;
+	private static String rcPrim;
+	private static String rcSec;
+	
+	private static String REGEX_PERM     = "[0-1]{3}";
+	private static String REGEX_FLAG      = "[-]{1}[vul]{1,3}";
+	private static String REGEX_CUSTRC = "[-]{2}[rcPrimSe]{5,6}";
 	
 	private static final double VERSION 	= 1.03;
 	private static final String PREAMBLE 	= "Directory Syncher Version " + VERSION + "\nMade and maintaned by Dan Martineau.\n\n";
 	private static final String PROG_NAME 	= "java -jar synch.jar";	//what one would use to call this program via commandline
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		read = true;
+		mod = false;
+		del = false;
+		verbose = false;
+		unidirectional = false;
+		saveLog = false;
+		rcPrim = SynchModule.DEFAULT_SYNCHRC;
+		rcSec = SynchModule.DEFAULT_SYNCHRC;
+		
+		
 		log = PREAMBLE + PROG_NAME + " ";
 		
 		for(int i = 0; i < args.length; i++)
 			log += args[i] + " ";
 		
 		log += "\n\nBeginning synch...\n";
-		verbose = false;
+		
+		initialLogLen = log.length();
+
+		//extract custom rc files if they are there
+		args = findCustRc(args);
 		
 		decodeArgs(args);
 	}
@@ -42,39 +71,201 @@ public class Main
 					Prin.err("Invalid arguments! Use --help for list of options.\n");
 				break;
 			case 2:
-				defaultSynch(args);
+				decodeDirs(args[0], args[1]);
+				startSynch();
 				break;
 			case 3:
+				threeArgs(args[0], args[1], args[2]);
+				startSynch();
+				break;
 			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-				customSynch(args);
+				fourArgs(args[0], args[1], args[2], args[3]);
+				startSynch();
 				break;
 			default:
 				Prin.err("Invalid arguments! Use --help for list of options.\n");
 		}
 	}
 	
-	private static void customSynch(String[] args)
+	private static void threeArgs(String one, String two, String three)
 	{
-		boolean read 		= true;
-		boolean modify 		= false;
-		boolean delete 		= false;
-		boolean unidirectional 	= false;
-		boolean saveLog 	= false;
-		String primrc 		= SynchModule.DEFAULT_SYNCHRC;
-		String secrc 		= SynchModule.DEFAULT_SYNCHRC;
-		String dir1 		= null;
-		String dir2 		= null;
-		
-		//if first has dash, then it is the flags
-		if(args[0].contains("-"))
+		if(match(REGEX_PERM, one))
 		{
-			for(int i = 0; i < args[0].length(); i++)
+			decodePerm(one);
+			decodeDirs(two, three);
+		}
+		else if(match(REGEX_FLAG, one))
+		{
+			decodeFlags(one);
+			decodeDirs(two, three);
+		}	
+		else
+		{
+			Prin.err("Invalid arguments! Use --help for list of options.\n");
+			error();
+		}
+	}
+	
+	private static void fourArgs(String one, String two, String three, String four)
+	{
+		if(match(REGEX_PERM, one) && match(REGEX_FLAG, two))
+		{
+			decodePerm(one);
+			decodeFlags(two);
+			decodeDirs(three, four);
+		}
+		else if(match(REGEX_FLAG, one) && match(REGEX_PERM, two))
+		{
+			decodeFlags(one);
+			decodePerm(two);
+			decodeDirs(three, four);
+		}	
+		else
+		{
+			Prin.err("Invalid arguments! Use --help for list of options.\n");
+			error();
+		}
+	}
+	
+	//comb through argument array to find end flags
+	private static String[] findCustRc(String[] items)
+	{
+		int nullCount = 0;
+		int itt = 0;
+		String[] newArr;
+		
+		for(int i = items.length-1; i >= 0; i--)
+		{
+			if(match(REGEX_CUSTRC, items[i]))
 			{
-				switch(args[0].charAt(i))
+				if(i == items.length-1)
+				{
+					Prin.err("Error: no directory given for: " + items[i] + "\n");
+					error();
+				}
+				else
+				{
+					decodeRc(items[i], items[i+1]);
+					items[i] = null;
+					items[i+1] = null;
+					nullCount += 2;
+				}
+			}
+		}
+		
+		//create new array with found flags removed
+		newArr = new String[items.length - nullCount];
+		
+		for(int i = 0; i < items.length; i++)
+		{
+			if(items[i] != null)
+			{
+				newArr[itt] = items[i];
+				itt++;
+			}
+		}
+		
+		return newArr;
+	}
+	
+	private static void startSynch()
+	{
+		assert dir1 != null : "The field \"dir1\" is null in startSynch(). This should not be possible.";
+		assert dir2 != null : "The field \"dir2\" is null in startSynch(). This should not be possible.";
+		
+		//check to ensure that the default rc files exist--create them if they don't
+		checkSynchrc(dir1);
+		checkSynchrc(dir2);
+		
+		//begin synch job
+		
+		//synch from primary to secondary
+		try
+		{
+			SynchModule part1 = new SynchModule(dir1, dir2, rcPrim, read, mod, del, verbose);
+		
+			log += part1.getLog();
+			
+			//synch from secondary to primary if not unidirectional
+			if(!unidirectional)
+			{
+				SynchModule part2 = new SynchModule(dir2, dir1, rcSec, read, mod, del, verbose);
+				
+				log += part2.getLog();
+			}
+		}
+		catch(Exception e)
+		{
+			//Prin.err("\n" + e.toString() + "\n");
+			//error();
+		}
+		
+		log += "\nFinished synch job!";
+		
+		if(verbose)
+			Prin.tln("\n\n" + log);
+		
+		saveLogFile(dir1);
+		saveLogFile(dir2);
+	}
+	
+	/**
+	*Set the permissions
+	*@param permissions string 
+	*/
+	private static void decodePerm(String permissions)
+	{
+		if(permissions.charAt(0) == '0')
+			read = false;
+		else if(permissions.charAt(0) == '1')
+			read = true;
+		
+		if(permissions.charAt(1) == '0')
+			mod = false;
+		else if(permissions.charAt(1) == '1')
+			mod = true;
+		
+		if(permissions.charAt(2) == '0')
+			del = false;
+		else if(permissions.charAt(2) == '1')
+			del = true;
+	}
+	
+	private static void decodeDirs(String path1, String path2)
+	{
+		if(!FileCMD.isDir(path1))
+		{
+			Prin.err("Invalid primary directory: " + path1 + "\n");
+			error();
+		}
+		
+		if(!FileCMD.existFile(path2))
+		{
+			log += "Creating secondary directory: " + path2 + "\n";
+			
+			//attempt to make secondary directory
+			boolean attempt = FileCMD.mkdirs(path2);
+			
+			if(attempt)
+				log += "Directory was successfully created!\n";
+			//exit if unsuccessful
+			else
+			{
+				log += "Failed to create directory. Ending synch...\n";
+				Prin.err("Could not create secondary directory.\n");
+				error();
+			}
+		}
+		
+		dir1 = path1;
+		dir2 = path2;
+	}
+	
+	private static void decodeFlags(String flags)
+	{
+		for(int i = 0; i < flags.length(); i++)
+			{
+				switch(flags.charAt(i))
 				{
 					case '-':
 						break;
@@ -88,314 +279,70 @@ public class Main
 						verbose = true;
 						break;
 					default:
-						Prin.err("Invalid flag series: " + args[0] + "\nRun with --help for list of flags.\n");
-						System.exit(-1);
+						Prin.err("Invalid flag series: " + flags + "\nRun with --help for list of flags.\n");
+						error();
 				}
 			}
-			
-			//see if next of args is permissions
-			if((args[1].contains("0") || args[1].contains("1")) && args[1].length() == 3 && !FileCMD.isDir(args[1]) && args.length >= 4)
-			{
-				if(args[1].charAt(0) == '0')
-					read = false;
-				else if(args[1].charAt(0) == '1')
-					read = true;
-				else
-				{
-					Prin.err("Invalid permissions entered: " + args[1] + "\n");
-					System.exit(-1);
-				}
-				
-				if(args[1].charAt(1) == '0')
-					modify = false;
-				else if(args[1].charAt(1) == '1')
-					modify = true;
-				else
-				{
-					Prin.err("Invalid permissions entered: " + args[1] + "\n");
-					System.exit(-1);
-				}
-				
-				if(args[1].charAt(2) == '0')
-					delete = false;
-				else if(args[1].charAt(2) == '1')
-					delete = true;
-				else
-				{
-					Prin.err("Invalid permissions entered: " + args[1] + "\n");
-					System.exit(-1);
-				}
-				
-				//check to see if next two args are dirs
-				if(!FileCMD.isDir(args[2]))
-				{
-					Prin.err("Invalid primary directory: " + args[2] + "\n");
-					System.exit(-1);
-				}
-				//test to see if secondary directory exists
-				if(!FileCMD.existFile(args[3]))
-				{
-					log += "Creating secondary directory: " + args[3] + "\n";
-					
-					//attempt to make secondary directory
-					boolean attempt = FileCMD.mkdirs(args[3]);
-					
-					if(attempt)
-						log += "Directory was successfully created!\n";
-					//exit if unsuccessful
-					else
-					{
-						log += "Failed to create directory. Ending synch...\n";
-						Prin.err("Could not create secondary directory.\n");
-						System.exit(-1);
-					}
-				}
-				
-				dir1 = args[2];
-				dir2 = args[3];
-			}
-			else
-			{
-				//check to see if next two args are dirs
-				if(!FileCMD.isDir(args[1]))
-				{
-					Prin.err("Invalid primary directory: " + args[1] + "\n");
-					System.exit(-1);
-				}
-				//test to see if secondary directory exists
-				if(!FileCMD.existFile(args[2]))
-				{
-					log += "Creating secondary directory: " + args[2] + "\n";
-					
-					//attempt to make secondary directory
-					boolean attempt = FileCMD.mkdirs(args[2]);
-					
-					if(attempt)
-						log += "Directory was successfully created!\n";
-					//exit if unsuccessful
-					else
-					{
-						log += "Failed to create directory. Ending synch...\n";
-						Prin.err("Could not create secondary directory.\n");
-						System.exit(-1);
-					}
-				}
-				
-				dir1 = args[1];
-				dir2 = args[2];
-			}
-		}
-		//if first has zeros or ones and a length of three, then it is permissions
-		else if((args[0].contains("0") || args[0].contains("1")) && args[0].length() == 3 && !FileCMD.isDir(args[0]))
+	}
+	
+	private static void decodeRc(String str1, String str2)
+	{
+		if(!FileCMD.existFile(str2))
 		{
-			if(args[0].charAt(0) == '0')
-					read = false;
-			else if(args[0].charAt(0) == '1')
-				read = true;
-			else
-			{
-				Prin.err("Invalid permissions entered: " + args[0] + "\n");
-				System.exit(-1);
-			}
-			
-			if(args[0].charAt(1) == '0')
-				modify = false;
-			else if(args[0].charAt(1) == '1')
-				modify = true;
-			else
-			{
-				Prin.err("Invalid permissions entered: " + args[0] + "\n");
-				System.exit(-1);
-			}
-			
-			if(args[0].charAt(2) == '0')
-				delete = false;
-			else if(args[0].charAt(2) == '1')
-				delete = true;
-			else
-			{
-				Prin.err("Invalid permissions entered: " + args[0] + "\n");
-				System.exit(-1);
-			}
-			
-			//check to see if next two args are dirs
-			if(!FileCMD.isDir(args[1]))
-			{
-				Prin.err("Invalid primary directory: " + args[1] + "\n");
-				System.exit(-1);
-			}
-			//test to see if secondary directory exists
-			if(!FileCMD.existFile(args[2]))
-			{
-				log += "Creating secondary directory: " + args[2] + "\n";
-				
-				//attempt to make secondary directory
-				boolean attempt = FileCMD.mkdirs(args[2]);
-				
-				if(attempt)
-					log += "Directory was successfully created!\n";
-				//exit if unsuccessful
-				else
-				{
-					log += "Failed to create directory. Ending synch...\n";
-					Prin.err("Could not create secondary directory.\n");
-					System.exit(-1);
-				}
-			}
-			
-			dir1 = args[1];
-			dir2 = args[2];
+			Prin.err("Invalid custom synchrc file: " + str2 + "\n");
+			error();
 		}
-		//else it must be a default synch with extra flags
-		else
+		else if(FileCMD.isDir(str2))
 		{
-			if(!FileCMD.isDir(args[0]))
-			{
-				Prin.err("Invalid primary directory: " + args[0] + "\n");
-				System.exit(-1);
-			}
-			//test to see if secondary directory exists
-			if(!FileCMD.existFile(args[1]))
-			{
-				log += "Creating secondary directory: " + args[1] + "\n";
-				
-				//attempt to make secondary directory
-				boolean attempt = FileCMD.mkdirs(args[1]);
-				
-				if(attempt)
-					log += "Directory was successfully created!\n";
-				//exit if unsuccessful
-				else
-				{
-					log += "Failed to create directory. Ending synch...\n";
-					Prin.err("Could not create secondary directory.\n");
-					System.exit(-1);
-				}
-			}
-			
-			dir1 = args[0];
-			dir2 = args[1];
+			Prin.err("Invalid custom synchrc file: " + str2 + "\n");
+			error();
 		}
 		
-		//handle the final args to see if they are extra flags
-		if(args.length >= 4 && args[args.length-2].equals("--rcPrim"))
+		switch(str1)
 		{
-			//make sure file exisits
-			if(!FileCMD.existFile(args[args.length-1]))
-			{
-				Prin.err("Custom synchrc file \"" + args[args.length-1] + "\" does not exist.\n");
-				System.exit(-1);
-			}
-			else
-				primrc = args[args.length-1];
-		}
-		else if(args.length >= 4 && args[args.length-2].equals("--rcSec"))
-		{
-			//make sure file exisits
-			if(!FileCMD.existFile(args[args.length-1]))
-			{
-				Prin.err("Custom synchrc file \"" + args[args.length-1] + "\" does not exist.\n");
-				System.exit(-1);
-			}
-			else
-				secrc = args[args.length-1];
-		}
-		
-		if(args.length >= 6 && args[args.length-4].equals("--rcPrim"))
-		{
-			//make sure file exisits
-			if(!FileCMD.existFile(args[args.length-3]))
-			{
-				Prin.err("Custom synchrc file \"" + args[args.length-3] + "\" does not exist.\n");
-				System.exit(-1);
-			}
-			else
-				primrc = args[args.length-3];
-		}
-		else if(args.length >= 6 && args[args.length-4].equals("--rcSec"))
-		{
-			//make sure file exisits
-			if(!FileCMD.existFile(args[args.length-3]))
-			{
-				Prin.err("Custom synchrc file \"" + args[args.length-3] + "\" does not exist.\n");
-				System.exit(-1);
-			}
-			else
-				secrc = args[args.length-3];
-		}
-		
-		//check to ensure that the rc files exist
-		checkSynchrc(dir1);
-		checkSynchrc(dir2);
-		
-		//begin synch job
-		
-		//synch from primary to secondary
-		SynchModule part1 = new SynchModule(dir1, dir2, primrc, read, modify, delete, verbose);
-		
-		log += part1.getLog();
-		
-		//synch from secondary to primary if not unidirectional
-		if(!unidirectional)
-		{
-			SynchModule part2 = new SynchModule(dir2, dir1, secrc, read, modify, delete, verbose);
-			
-			log += part2.getLog() + "\nFinished synch job!";
-		}
-		
-		if(verbose)
-			Prin.tln("\n" + log);
-		
-		if(saveLog)
-		{
-			saveLogFile(dir1);
-			saveLogFile(dir2);
+			case "--rcPrim":
+				rcPrim = str2;
+				break;
+			case "--rcSec":
+				rcSec = str2;
+				break;
+			default:
+				Prin.err("Invalid flag: " + str1 + "\n");
+				error();
 		}
 	}
 	
-	private static void defaultSynch(String[] args)
+	private static boolean match(String regex, String container)
 	{
-		//test to make sure primary exisits
-		if(!FileCMD.existFile(args[0]))
-		{
-			Prin.err("Primary directory \"" + args[0] + "\" does not exist.\n");
-			System.exit(-1);
-		}
+		Pattern pat = Pattern.compile(regex);
+		Matcher match = pat.matcher(container);
 		
-		//test to see if secondary directory exists
-		if(!FileCMD.existFile(args[1]))
-		{
-			log += "Creating secondary directory: " + args[1] + "\n";
-			
-			//attempt to make secondary directory
-			boolean attempt = FileCMD.mkdirs(args[1]);
-			
-			if(attempt)
-				log += "Directory was successfully created!\n";
-			//exit if unsuccessful
-			else
-			{
-				log += "Failed to create directory. Ending synch...\n";
-				Prin.err("Could not create secondary directory.\n");
-				System.exit(-1);
-			}
-		}
+		return match.find();
+	}
+	
+	private static void error()
+	{
+		if(dir1 != null)
+			saveLogFile(dir1);
+		if(dir2 != null)
+			saveLogFile(dir2);
 		
-		//ensure that both dirs have synchrc files
-		checkSynchrc(args[0]);
-		checkSynchrc(args[1]);
+		System.exit(-1);
+	}
+	
+	private static void checkSynchrc(String path)
+	{
+		final String PARENT_DIR = FileCMD.getName(path);
+		final String DEFAULT_RC_CONT = "#This is your synchrc file\n\n#This line prevents your synchrc files from overwriting eachother across directories.\n000 " + PARENT_DIR + ",synchrc\n\n#The following two lines are essential when running a job on the parent directory of a drive in Windows.\n000 " + PARENT_DIR + ",System Volume Information\n000 " + PARENT_DIR + ",$RECYCLE.BIN";
 		
-		//synch from primary to secondary
-		SynchModule part1 = new SynchModule(args[0], args[1], SynchModule.DEFAULT_SYNCHRC);
+		if(path.charAt(path.length()-1) == File.separatorChar)
+			path += SynchModule.DEFAULT_SYNCHRC;
+		else
+			path += File.separatorChar + SynchModule.DEFAULT_SYNCHRC;
 		
-		log += part1.getLog();
-		
-		//synch from secondary to primary
-		SynchModule part2 = new SynchModule(args[1], args[0], SynchModule.DEFAULT_SYNCHRC);
-		
-		log += part2.getLog() + "\nFinished synch job!";
-		
-		Prin.tln(log);
+		//cehck to see if synchrc exisits
+		if(!FileCMD.existFile(path))
+			FileCMD.writeFile(DEFAULT_RC_CONT, path);
 	}
 	
 	private static void displayOptions()
@@ -414,33 +361,21 @@ public class Main
 		Prin.tln("\nNote: The default permissions are \"100.\"");
 	}
 	
-	private static void checkSynchrc(String path)
-	{
-		final String PARENT_DIR = FileCMD.getName(path);
-		final String DEFAULT_RC_CONT = "#This is your synchrc file\n\n#This line prevents your synchrc files from overwriting eachother across directories.\n000 " + PARENT_DIR + ",synchrc\n\n#The following two lines are essential when running a job on the parent directory of a drive in Windows.\n000 " + PARENT_DIR + ",System Volume Information\n000 " + PARENT_DIR + ",$RECYCLE.BIN";
-		
-		if(path.charAt(path.length()-1) == File.separatorChar)
-			path += SynchModule.DEFAULT_SYNCHRC;
-		else
-			path += File.separatorChar + SynchModule.DEFAULT_SYNCHRC;
-		
-		//cehck to see if synchrc exisits
-		if(!FileCMD.existFile(path))
-			FileCMD.writeFile(DEFAULT_RC_CONT, path);
-	}
-
 	private static void saveLogFile(String path)
 	{
-		if(path.charAt(path.length()-1) == File.separatorChar)
-			path = path.substring(0, path.length()-1);
-		
-		String fileName = "synchlog_" + (new Date(System.currentTimeMillis())).toString() + ".txt";
-		fileName = fileName.replace(':', '_');
-		String destination = path + File.separatorChar + "synchlogs" + File.separatorChar + fileName;
-		
-		if(!FileCMD.isDir(path + File.separatorChar + "synchlogs"))
-			FileCMD.mkdirs(path + File.separatorChar + "synchlogs");
-		
-		FileCMD.writeFile(log, destination);
+		if(log.length() > initialLogLen && saveLog)
+		{
+			if(path.charAt(path.length()-1) == File.separatorChar)
+				path = path.substring(0, path.length()-1);
+			
+			String fileName = "synchlog_" + (new Date(System.currentTimeMillis())).toString() + ".txt";
+			fileName = fileName.replace(':', '_');
+			String destination = path + File.separatorChar + "synchlogs" + File.separatorChar + fileName;
+			
+			if(!FileCMD.isDir(path + File.separatorChar + "synchlogs"))
+				FileCMD.mkdirs(path + File.separatorChar + "synchlogs");
+			
+			FileCMD.writeFile(log, destination);
+		}
 	}
 }
