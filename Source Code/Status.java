0d4ef6227083c0667c815f3d37a279a62da1baab
justdan325 <dannyjames16@hotmail.com>
2019-10-22 20:23:06 -0400

    Added Status to make verbose mode a little cleaner. Not yet implememnted in SynchModule.

---
+++ Source Code/Status.java
@@ -0,0 +1,212 @@
+/**
+*Class is a singleton that manages the status of the state of SynchModule when running a job.
+*
+*@author Dan Martineau
+*@version 1.0
+*@since 2.0
+*/
+
+public class Status
+{
+	private static String job;			//Running job from dir a to dir b
+	private static String mode;			//Read/Mod/Delete 
+	private static String file;			//current file being processed
+	private static String dir;			//current directory
+	private static String progress;		//ASCII progress meter
+	private static int totalLocal;		//total number of files in the subdir
+	private static int total;			//total number of files in all subdirs beneath parent dir
+	private static int curr;				//number of current file being processed
+	private static int currLocal;			//number of local current file being processed
+	private static boolean printOnUpdate; 	//true if status should print itself every time certain mutators are called
+	private static boolean initialized;	//true if there is an instance of Status
+	private static Status status;			//instance of this class
+		
+	/*PUBLIC CONSTANTS*/
+	public static final String MODE_READ = "Searching for match";
+	public static final String MODE_MOD = "Checking for modifications";
+	public static final String MODE_DEL = "Assesing deletion";
+	public static final String MODE_IDLE = "Idle";
+	
+	/*PRIVATE CONSTANTS*/
+	private static final int PROG_LEN = 30;
+	private static final String PROG_BLANK = "-";
+	private static final String PROG_FILL = "=";
+	
+	private Status()
+	{
+		total = 0;
+		printOnUpdate = false;
+		initialized = true;
+		
+		setIdle();
+		
+		progress();
+	}
+	
+	/*ACCESSORS*/
+	
+	/**
+	*Returns the single instance of this class
+	*@return this
+	*/
+	public static Status getStatus()
+	{
+		if(!initialized)
+			status = new Status();
+		
+		return status;
+	}
+	
+	/*MUTATORS*/
+	
+	/**
+	*Set the current directory being processed as well as the number of files/dirs inside of it.
+	*@param directory
+	*@param total files inside
+	*/
+	public void setDir(String dir, int totalLocal)
+	{
+		this.dir = dir;
+		this.totalLocal = totalLocal;
+		curr++;
+		currLocal = 0;
+		progress();
+		
+		if(printOnUpdate)
+		{
+			Prin.clearAll();
+			Prin.tln(toString());
+		}
+	}
+	
+	/**
+	*Set current file being processed.
+	*@param file name (not path)
+	*/
+	public void setFile(String file)
+	{
+		this.file = file;
+		curr++;
+		currLocal++;
+		progress();
+		
+		if(printOnUpdate)
+		{
+			Prin.clearAll();
+			Prin.tln(toString());
+		}
+	}
+	
+	/**
+	*Set the total number of files and subdirectories to be processed (include parent dir)
+	*@total files and subdirs
+	*/
+	public void setTotal(int total)
+	{
+		this.total = total;
+	}
+	
+	/**
+	*Set the job being run
+	*@the job
+	*/
+	public void setJob(String job)
+	{
+		this.job = job;
+	}
+	
+	/**
+	*Set the processing mode.
+	*@param processing mode (must be one of the constants)
+	*/
+	public void setMode(String newMode)
+	{
+		switch(newMode)
+		{
+			case MODE_READ:
+			case MODE_MOD:
+			case MODE_DEL:
+			case MODE_IDLE:
+				this.mode = newMode;
+				break;
+			default:
+				throw new RuntimeException("Invalid mode passed into setMode(): " + newMode);
+		}
+		
+		if(printOnUpdate)
+		{
+			Prin.clearAll();
+			Prin.tln(toString());
+		}
+	}
+	
+	/**
+	*Sets the status indicator to idle and resets counters--do not call in middle of a job!
+	*/
+	public void setIdle()
+	{
+		job = "Not running. . .";
+		mode = MODE_IDLE;
+		file = "--";
+		dir = "--";
+		totalLocal = 0;
+		currLocal = 0;
+		curr = 0;
+	}
+	
+	/**
+	*Set true to automatically print each time certain fields are updated.
+	*@param true or false
+	*/
+	public void setPrintOnUpdate(boolean printOnUpdate)
+	{
+		this.printOnUpdate = printOnUpdate;
+	}
+	
+	private void progress()
+	{
+		assert total >= curr : "total < curr in Status! total: " + total + " curr: " + curr;
+		
+		if(total > 0)
+		{
+			int fillNo = (int)(PROG_LEN*(((double)curr/total)));
+			
+			progress = "[";
+			
+			for(int i = 0; i < fillNo; i++)
+				progress += PROG_FILL;
+			
+			for(int i = 0; i < PROG_LEN-fillNo; i++)
+				progress += PROG_BLANK;
+			
+			progress += "]";
+		}
+		else
+		{
+			progress = "[";
+			
+			for(int i = 0; i < PROG_LEN; i++)
+				progress += PROG_BLANK;
+			
+			progress += "]";
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		String str = "";
+		
+		if(initialized)
+		{
+			str += "Job: " + job + "\n";
+			str += "Directory: " + dir + "\n";
+			str += "Processing " + currLocal + " out of " + totalLocal + "\n";
+			str += "File: " + file + "\n";
+			str += "Mode: " + mode + "\n\n";
+			str += "Progress: " + progress + "\n";
+		}
+		
+		return str;
+	}
+}
--- Source Code/UnitTest.java
+++ Source Code/UnitTest.java
@@ -5,176 +5,19 @@ public class UnitTest
 {
 	public static void main(String[] args)
 	{
-		Prin.tln("Returns: " + FileCMD.getCanonPath("/home/dan/Programming Projects/testbed/dir1/dir3/"));
-	}
-	
-	public static void addWildcardMatches(String wild, boolean read, boolean modify, boolean delete)
-	{
-		addWildcardMatchesHelper(wild, wild, read, modify, delete);
-	}
-	
-	//WILL HAVE TO BE CALLED EXCLUSIVLEY IN CONSTRUCTOR OF NODE
-	public static void addWildcardMatchesHelper(String wild, String orig, boolean read, boolean modify, boolean delete) 
-	{
-		String rootPath;
-		String restOfWild;
-		boolean recursive = false;
-		String[] filesAndDirs;
-		String[] subDirs;
+		Status status = Status.getStatus();
+		String[] files = FileCMD.listAll(args[0]);
 		
-		if(wild.contains("*"))
-		{
-			rootPath = wild.substring(0, wild.indexOf("*"));
-			if(!FileCMD.existFile(rootPath) && rootPath.contains(File.separatorChar + ""))
-				rootPath = rootPath.substring(0, rootPath.lastIndexOf(File.separatorChar + "")+1);
-		}
-		else
-		{
-			rootPath = wild;
-			Prin.tln("Add: " + rootPath); //addNode(new Node(rootPath, read, modify, delete);
-		}
-		
-		//Do not continue if the rootPath does not exist
-		if(FileCMD.isDir(rootPath))
-		{
-			if(wild.charAt(rootPath.length()) == '*')
-				restOfWild = wild.substring(rootPath.length());
-			else
-				restOfWild = wild.substring(wild.indexOf("*"));
-			
-			filesAndDirs = null;
-			
-			//NullPointerException here means that user does not have access to a subdirectory. The error will be given in Synchmodule, so ignore here.
-			try{filesAndDirs = FileCMD.listAll(rootPath);}
-			catch(NullPointerException e){return;}
-			
-			if(wild.length() > wild.indexOf("*")+1)
-				recursive = wild.charAt(wild.indexOf("*")+1) == '*';
-			
-			//get all files and subdirs in rootPath
-			//run them through match
-			//if they match, make them new Nodes and add them
-			for(int i = 0; i < filesAndDirs.length; i++)
-			{
-				//Prin.tln(orig + " == " + filesAndDirs[i] + "?");
-				//Prin.tln(match(orig, filesAndDirs[i]) + "");
-				if(match(orig, filesAndDirs[i]))
-					Prin.tln("Add: " + filesAndDirs[i]); //addNode(new Node(filesAndDirs[i], read, modify, delete);
-			}
-			
-			//if recursive, search through all subdirectories
-			if(recursive || restOfWild.contains(File.separatorChar + ""))
-			{
-				subDirs = FileCMD.listDirs(rootPath);
-				
-				for(int i = 0; i < subDirs.length; i++)
-				{
-					wild = subDirs[i] + restOfWild;
-					/*if(restOfWild.equals(""))
-						wild = subDirs[i] + "*";*/
-					//Prin.tln("wild: " + wild);
-					//IF restOfWild HAS NO WILD CARDS, DO STUFF AND ADD RESULT
-					addWildcardMatchesHelper(wild, orig, read, modify, delete);
-				}
-			}
-		}
-	}
-	
-	public static boolean match(String wild, String path)
-	{
-		ArrayList<Integer> locations = new ArrayList<Integer>();
-		ArrayList<String> pieces = new ArrayList<String>();
-		String subStr = wild;
-		int index = -1;
-		int prev = 0;
-		int curr = -1;
-		boolean match = false;
-		
-		if(wild.contains("*"))
-		{
-			while(wild.contains("**"))
-			{
-				index = wild.indexOf("**");
-				wild = wild.substring(0,index+1) + wild.substring(index+2);
-			}
-			index = -1;
-			
-			//find locations of all wild cards
-			while(index < wild.length()-1)
-			{
-				subStr = wild.substring(index+1);
-				index = subStr.indexOf("*");
-				
-				if(index == -1)
-					break;
-				
-				index = (wild.length() - subStr.length()) + index;
-				
-				locations.add(index);
-			}
-			
-			//chop wild into pieces
-			for(int i = 0; i < locations.size(); i++)
-			{
-				prev = curr+1;
-				curr = locations.get(i);
-				
-				if(!wild.substring(prev, curr).equals(""))
-					pieces.add(wild.substring(prev, curr));
-			}
-			//add last piece
-			if(curr < wild.length()-1)
-			{
-				pieces.add(wild.substring(curr+1));
-			}
-			
-			//see if all of the pieces are contained within the path in the correct order
-			curr = 0;
-			for(int i = 0; i < pieces.size(); i++)
-			{
-				prev = curr;
-				curr = prev + pieces.get(i).length();
-				
-				match = false;
 
-				while(curr <= path.length() && !match)
-				{
-					if(curr == path.length() || (i == pieces.size()-1 && wild.charAt(wild.length()-1) != '*'))
-					{
-						//Prin.tln("|" + path.substring(prev) + "|");
-						//Prin.tln("|" + pieces.get(i) + "|");
-						if(path.substring(prev).equals(pieces.get(i)))
-							match = true;
-						else
-							match = false;
-					}
-					else
-					{
-						//Prin.tln("|" + path.substring(prev, curr) + "|");
-						//Prin.tln("|" + pieces.get(i) + "|");
-						if(path.substring(prev, curr).equals(pieces.get(i)))
-							match = true;
-						else
-							match = false;
-					}
-					
-					//Prin.tln("" + match);
-					
-					prev++;
-					curr++;
-				}
-				
-				curr--;
-				
-				if(!match)
-					break;
-			}
-		}
-		else
+		status.setTotal(files.length+1);
+		status.setDir(args[0], files.length);
+		status.setPrintOnUpdate(true);
+
+		for(String file: files)
 		{
-			match = wild.equals(path);
+			status.setFile(file);
+			status.setMode(Status.MODE_READ);
+			Prin.pause(20);
 		}
-		
-		return match;
 	}
 }
